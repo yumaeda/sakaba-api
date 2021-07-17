f268133324460a9b94f4fd083ecd40a0ee18df38
Yukitaka Maeda <yumaeda@gmail.com>
2021-07-17 11:52:32 +0900

    Add lambda function for retrieving menus

---
+++ get_menus/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./clean.sh
+
+pip3 install --target ./package pymysql
+cd package
+zip -r9 ${OLDPWD}/get_menus.zip .
+cd $OLDPWD
+zip -g get_menus.zip lambda_function.py
+
+aws lambda update-function-code \
+    --function-name getMenus \
+    --zip-file fileb://get_menus.zip
---
+++ get_menus/lambda_function.py
@@ -0,0 +1,51 @@
+# System Module
+import json
+import logging
+import os
+import sys
+
+# 3rd Party Module
+import pymysql
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(
+        host=os.environ['DB_HOST'],
+        user=os.environ['DB_USER'],
+        passwd=os.environ['DB_PASSWORD'],
+        db=os.environ['DB_NAME'],
+        connect_timeout=10,
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+except pymysql.MySQLError as e:
+    logger.error('ERROR: Could not connect to MariaDB instance.')
+    logger.error(e)
+    sys.exit()
+
+logger.info('SUCCESS: Connection to RDS MariaDB instance succeeded')
+
+SQL_STMT = """
+SELECT to_base64(UuidFromBin(restaurant_id)) AS restaurant_id,
+       name,
+       name_jpn,
+       category,
+       price
+  FROM menus
+ ORDER BY category ASC, price ASC 
+"""
+ 
+def lambda_handler(event, context):
+    """
+    This function fetches content from MySQL RDS instance
+    """
+    entities = []
+    with conn.cursor() as cursor:
+        cursor.execute(SQL_STMT)
+        entities = cursor.fetchall()
+    return {
+        'statusCode': 200,
+        'body': json.dumps(entities)
+    }
