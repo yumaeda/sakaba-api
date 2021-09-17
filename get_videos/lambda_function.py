e4451078bde49e985b23239c0e715c0df3af0eba
Yukitaka Maeda <yumaeda@gmail.com>
2021-09-17 16:31:26 +0900

    Remove duplicate code for getVideos lambda

--- get_videos/lambda_function.py
+++ get_videos/lambda_function.py
@@ -1,32 +1,6 @@
 # System Module
 import json
-import os
-import logging
-import sys
-
-# 3rd Party Module
-import pymysql
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-try:
-    conn = pymysql.connect(
-        host=os.environ['DB_HOST'],
-        user=os.environ['DB_USER'],
-        passwd=os.environ['DB_PASSWORD'],
-        db=os.environ['DB_NAME'],
-        connect_timeout=10,
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor,
-        autocommit=True
-    )
-except pymysql.MySQLError as e:
-    logger.error('ERROR: Could not connect to MariaDB instance.')
-    logger.error(e)
-    sys.exit()
-
-logger.info('SUCCESS: Connection to RDS MariaDB instance succeeded')
+from common import conn, get_response, STATUS_CODE_OK
 
 def lambda_handler(event, context):
     """
@@ -47,7 +21,4 @@ def lambda_handler(event, context):
             sql += ' WHERE r.id = UuidToBin(\'{}\')'.format(restaurant_id)
         cursor.execute(sql)
         videos = cursor.fetchall()
-    return {
-        'statusCode': 200,
-        'body': json.dumps(videos)
-    }
+    return get_response(STATUS_CODE_OK, json.dumps(videos))
