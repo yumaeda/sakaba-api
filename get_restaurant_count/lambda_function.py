fd3c6aece5e94ece7abdbaa4afca065974daef8f
Yukitaka Maeda <yumaeda@gmail.com>
2021-09-17 14:58:18 +0900

    Remove duplicate code for getRestaurantCount lambda

--- get_restaurant_count/lambda_function.py
+++ get_restaurant_count/lambda_function.py
@@ -1,32 +1,5 @@
-# System Module
 import json
-import logging
-import os
-import sys
-
-# 3rd Party Module
-import pymysql
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-try:
-    conn = pymysql.connect(
-        host=os.environ['DB_HOST'],
-        user=os.environ['DB_USER'],
-        passwd=os.environ['DB_PASSWORD'],
-        db=os.environ['DB_NAME'],
-        connect_timeout=10,
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor,
-        autocommit=True
-    )
-except pymysql.MySQLError as e:
-    logger.error('ERROR: Could not connect to MariaDB instance.')
-    logger.error(e)
-    sys.exit()
-
-logger.info('SUCCESS: Connection to RDS MariaDB instance succeeded')
+from common import conn, get_response, STATUS_CODE_OK
 
 def lambda_handler(event, context):
     """
@@ -47,7 +20,4 @@ SELECT area,
         cursor.execute(sql)
         shop_counts = cursor.fetchall()
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(shop_counts)
-    }
+    return get_response(STATUS_CODE_OK, json.dumps(shop_counts))
