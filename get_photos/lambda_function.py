134769f16a779cff9d6f1fff6a1c4817b07440a4
Yukitaka Maeda <yumaeda@gmail.com>
2021-09-17 16:34:46 +0900

    Remove duplicate code for getPhotos lambda

--- get_photos/lambda_function.py
+++ get_photos/lambda_function.py
@@ -1,32 +1,5 @@
-# System Module
 import json
-import logging
-import os
-import sys
-
-# 3rd Party Module
-import pymysql
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-try:
-    conn = pymysql.connect(
-        host=os.environ['DB_HOST'],
-        user=os.environ['DB_USER'],
-        passwd=os.environ['DB_PASSWORD'],
-        db=os.environ['DB_NAME'],
-        connect_timeout=10,
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor,
-        autocommit=True
-    )
-except pymysql.MySQLError as e:
-    logger.error('ERROR: Could not connect to MariaDB instance.')
-    logger.error(e)
-    sys.exit()
-
-logger.info('SUCCESS: Connection to RDS MariaDB instance succeeded')
+from common import conn, get_response, STATUS_CODE_OK
 
 def lambda_handler(event, context):
     """
@@ -47,11 +20,7 @@ def lambda_handler(event, context):
         if 'restaurant_id' in event:
             restaurant_id = event['restaurant_id']
             sql += ' WHERE r.id = UuidToBin(\'{}\')'.format(restaurant_id)
-
         cursor.execute(sql)
         photos = cursor.fetchall()
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(photos)
-    }
+    return get_response(STATUS_CODE_OK, json.dumps(photos))
