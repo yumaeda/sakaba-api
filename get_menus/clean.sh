da240f719e23aca1c04539196f7625ebdeb47e86
Yukitaka Maeda <yumaeda@gmail.com>
2021-09-17 16:22:40 +0900

    Remove duplicate code for getMenus lambda

--- get_menus/clean.sh
+++ get_menus/clean.sh
@@ -2,5 +2,6 @@
 
 rm -rf package
 rm -rf __pycache__
+rm common.py
 rm get_menus.zip
 rm output.json
--- get_menus/deploy.sh
+++ get_menus/deploy.sh
@@ -2,11 +2,14 @@
 
 ./clean.sh
 
+cp ../common.py ./common.py
+
 pip3 install --target ./package pymysql
 cd package
 zip -r9 ${OLDPWD}/get_menus.zip .
 cd $OLDPWD
 zip -g get_menus.zip lambda_function.py
+zip -g get_menus.zip common.py
 
 aws lambda update-function-code \
     --function-name getMenus \
