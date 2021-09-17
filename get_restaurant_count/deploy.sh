f5c43c131f7881016f7f37e56b198ddce7296e2d
Yukitaka Maeda <yumaeda@gmail.com>
2021-09-17 16:14:20 +0900

    Fix lambda errors

--- get_restaurant_count/deploy.sh
+++ get_restaurant_count/deploy.sh
@@ -9,6 +9,7 @@ cd package
 zip -r9 ${OLDPWD}/get_restaurant_count.zip .
 cd $OLDPWD
 zip -g get_restaurant_count.zip lambda_function.py
+zip -g get_restaurant_count.zip common.py
 
 aws lambda update-function-code \
     --function-name getRestaurantCount \
--- get_restaurants/deploy.sh
+++ get_restaurants/deploy.sh
@@ -9,6 +9,7 @@ cd package
 zip -r9 ${OLDPWD}/get_restaurants.zip .
 cd $OLDPWD
 zip -g get_restaurants.zip lambda_function.py
+zip -g get_restaurants.zip common.py
 
 aws lambda update-function-code \
     --function-name getRestaurants \
