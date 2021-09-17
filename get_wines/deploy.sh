58d22e8cd98670acc334e219de0289f58d86a33b
Yukitaka Maeda <yumaeda@gmail.com>
2021-09-17 16:19:13 +0900

    Remove duplicate code for getWines lambda

--- get_wines/deploy.sh
+++ get_wines/deploy.sh
@@ -2,11 +2,14 @@
 
 ./clean.sh
 
+cp ../common.py ./common.py
+
 pip3 install --target ./package pymysql
 cd package
 zip -r9 ${OLDPWD}/get_wines.zip .
 cd $OLDPWD
 zip -g get_wines.zip lambda_function.py
+zip -g get_wines.zip common.py
 
 aws lambda update-function-code \
     --function-name getWines \
