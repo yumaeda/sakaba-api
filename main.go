d28b2b79e8f4ebf6a0b9d208402239296370cc7e
yumaeda <yumaeda@gmail.com>
2021-11-26 14:30:47 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-    "fmt"
     "os"
     "github.com/gin-gonic/gin"
     "net/http"
 )
 
 func main() {
-    spring_app_json = os.Getenv("SPRING_APPLICATION_JSON")
+    spring_app_json := os.Getenv("SPRING_APPLICATION_JSON")
     engine:= gin.Default()
     engine.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
