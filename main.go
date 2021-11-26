ff13918bb3bc2fc9656d012eb95f520203b3a4ad
yumaeda <yumaeda@gmail.com>
2021-11-26 14:29:34 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "net/http"
+import (
+    "fmt"
+    "os"
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
 
 func main() {
+    spring_app_json = os.Getenv("SPRING_APPLICATION_JSON")
     engine:= gin.Default()
     engine.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
-            "message": "hello world",
+            "message": "hello world: " + spring_app_json,
         })
     })
     engine.Run(":8080")
