787fb617117ad0694bb1f380d746bdf23418138c
Yukitaka Maeda <yumaeda@gmail.com>
2021-11-27 16:10:19 +0900

    Debug CORS

--- src/main.go
+++ src/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"time"
+	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"sakaba.link/api/src/controller"
 )
 
+func CORS(ctx *gin.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Methods", "GET, OPTIONS")
+	ctx.Header("Access-Control-Allow-Headers", "*")
+	ctx.Header("Content-Type", "application/json")
+
+	// Second, we handle the OPTIONS problem
+	if ctx.Request.Method != "OPTIONS" {
+		ctx.Next()
+	} else {
+
+		// Everytime we receive an OPTIONS request,
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real
+		// request using any other method than OPTIONS
+		ctx.AbortWithStatus(http.StatusOK)
+	}
+}
+
+/*
 func getCorsConfig() cors.Config {
 	return cors.Config{
 		AllowMethods: []string{
@@ -26,13 +45,14 @@ func getCorsConfig() cors.Config {
 		MaxAge: 12 * time.Hour,
 	}
 }
+*/
 
 func main() {
 	homeController := controller.HomeController{}
 	videoController := controller.VideoController{}
 
 	router := gin.Default()
-	router.Use(cors.New(getCorsConfig()))
+	router.Use(CORS)
 	router.GET("/", homeController.Index)
 	router.GET("/videos/", videoController.GetAllVideos)
 	router.GET("/videos/:id", videoController.GetVideosByRestaurantId)
