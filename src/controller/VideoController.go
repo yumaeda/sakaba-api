83e71a006d90e020abccbf78fae6358cab9db943
Yukitaka Maeda <yumaeda@gmail.com>
2021-11-27 14:43:27 +0900

    Add CORS settings

--- src/controller/VideoController.go
+++ src/controller/VideoController.go
@@ -10,11 +10,17 @@ import (
 
 type VideoController struct{}
 
+func setCORS(ctx *gin.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "https://sakaba.link")
+	ctx.Header("Access-Control-Allow-Methods", "GET, OPTIONS")
+}
+
 func (c *VideoController) GetAllVideos(ctx *gin.Context) {
 	allVideos := []model.Video{}
 	db := infrastructure.ConnectToDB()
 	db.Raw("SELECT id, UuidFromBin(restaurant_id) AS restaurant_id, name, url FROM videos ORDER BY name").Scan(&allVideos)
 
+	setCORS(ctx)
 	ctx.JSON(http.StatusOK, gin.H{
 		"statusCode": 200,
 		"body":       allVideos,
@@ -28,6 +34,7 @@ func (c *VideoController) GetVideosByRestaurantId(ctx *gin.Context) {
 	db := infrastructure.ConnectToDB()
 	db.Table("videos").Select("name", "url").Where("restaurant_id = ?", restaurantId).Scan(&videos)
 
+	setCORS(ctx)
 	ctx.JSON(http.StatusOK, gin.H{
 		"statusCode": 200,
 		"body":       videos,
