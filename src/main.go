5f42143d86634a0df7cd210b799380753a72abd1
Yukitaka Maeda <yumaeda@gmail.com>
2021-11-28 17:44:09 +0900

    Harden CORS setting

--- src/main.go
+++ src/main.go
@@ -1,51 +1,23 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"sakaba.link/api/src/controller"
 )
 
 func CORS(ctx *gin.Context) {
-	ctx.Header("Access-Control-Allow-Origin", "*")
-	ctx.Header("Access-Control-Allow-Methods", "GET, OPTIONS")
+	ctx.Header("Access-Control-Allow-Origin", "https://sakaba.link")
+	ctx.Header("Access-Control-Allow-Methods", "GET")
 	ctx.Header("Access-Control-Allow-Headers", "*")
 	ctx.Header("Content-Type", "application/json")
-
-	// Second, we handle the OPTIONS problem
-	if ctx.Request.Method != "OPTIONS" {
-		ctx.Next()
-	} else {
-
-		// Everytime we receive an OPTIONS request,
-		// we just return an HTTP 200 Status Code
-		// Like this, Angular can now do the real
-		// request using any other method than OPTIONS
-		ctx.AbortWithStatus(http.StatusOK)
-	}
+	/*
+		if ctx.Request.Method != "OPTIONS" {
+			ctx.Next()
+		} else {
+			ctx.AbortWithStatus(http.StatusOK)
+		}
+	*/
 }
-
-/*
-func getCorsConfig() cors.Config {
-	return cors.Config{
-		AllowMethods: []string{
-			"GET",
-			"OPTIONS",
-		},
-		AllowHeaders: []string{
-			"Access-Control-Allow-Headers",
-			"Content-Type",
-			"Content-Length",
-			"Accept-Encoding",
-		},
-		AllowOrigins: []string{
-			"https://sakaba.link",
-		},
-		MaxAge: 12 * time.Hour,
-	}
-}
-*/
 
 func main() {
 	homeController := controller.HomeController{}
