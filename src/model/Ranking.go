c24ca515904554b0a35e1d5750279f783a29ff2b
Yukitaka Maeda <yumaeda@gmail.com>
2021-12-09 11:49:29 +0900

    Add missing restaurant field

--- src/model/Ranking.go
+++ src/model/Ranking.go
@@ -3,6 +3,7 @@ package model
 type Ranking struct {
 	Dish          string `json:"dish"`
 	Rank          int    `json:"rank"`
+	Restaurant    string `json:"restaurant"`
 	RestaurantId  string `json:"restaurant_id"`
 	Photo         string `json:"photo"`
 	RestaurantUrl string `json:"restaurant_url"`
