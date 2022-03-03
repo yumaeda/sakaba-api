e3dd41500bb2d1d31d5bee5cb4c595b9ebdc2ac6
Yukitaka Maeda <yumaeda@gmail.com>
2022-03-03 22:39:15 +0900

    Apply repository pattern to Photo

--- src/controller/PhotoController.go
+++ src/controller/PhotoController.go
@@ -4,26 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"sakaba.link/api/src/infrastructure"
-	"sakaba.link/api/src/model"
+	"sakaba.link/api/src/repository"
 )
 
 type PhotoController struct{}
 
 func (c *PhotoController) GetAllPhotos(ctx *gin.Context) {
-	allPhotos := []model.Photo{}
-	db := infrastructure.ConnectToDB()
-
-	db.Raw(`SELECT UuidFromBin(r.id) AS restaurant_id,
-                       CONCAT(p.name, '.jpg') AS image,
-                       CONCAT(p.name, '.webp') AS image_webp,
-                       CONCAT(p.name, '_thumbnail.jpg') AS thumbnail,
-                       CONCAT(p.name, '_thumbnail.webp') AS thumbnail_webp
-                  FROM photos AS p
-                  JOIN restaurants AS r
-                    ON p.restaurant_id = r.id
-                 ORDER BY p.create_time DESC`).Scan(&allPhotos)
-	infrastructure.CloseDB(db)
+	photoRepository := repository.PhotoRepository{}
+	allPhotos := photoRepository.GetAllPhotos()
 
 	ctx.JSON(http.StatusOK, gin.H{
 		"statusCode": 200,
---
+++ src/repository/PhotoRepository.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"sakaba.link/api/src/infrastructure"
+	"sakaba.link/api/src/model"
+)
+
+type PhotoRepository struct{}
+
+func (c *PhotoRepository) GetAllPhotos() []model.Photo {
+	allPhotos := []model.Photo{}
+	db := infrastructure.ConnectToDB()
+	db.Raw(`SELECT UuidFromBin(r.id) AS restaurant_id,
+                       CONCAT(p.name, '.jpg') AS image,
+                       CONCAT(p.name, '.webp') AS image_webp,
+                       CONCAT(p.name, '_thumbnail.jpg') AS thumbnail,
+                       CONCAT(p.name, '_thumbnail.webp') AS thumbnail_webp
+                  FROM photos AS p
+                  JOIN restaurants AS r
+                    ON p.restaurant_id = r.id
+                 ORDER BY p.create_time DESC`).Scan(&allPhotos)
+	infrastructure.CloseDB(db)
+
+	return allPhotos
+}
